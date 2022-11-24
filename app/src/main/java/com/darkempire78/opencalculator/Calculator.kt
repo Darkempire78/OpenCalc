0b21076c39768cf71ace585b9fac7e00fc59ab9b
Darkempire <50015928+Darkempire78@users.noreply.github.com>
2022-11-24 22:37:12 +0100

    add decimal factorials

--- app/src/main/java/com/darkempire78/opencalculator/Calculator.kt
+++ app/src/main/java/com/darkempire78/opencalculator/Calculator.kt
@@ -6,12 +6,41 @@ import kotlin.math.*
 class Calculator {
 
     fun factorial(number: Double): Double {
-        var factorial = BigInteger("1")
-        for (i in 1..number.toInt()) {
-            factorial *= i.toBigInteger()
+        val decimalPartOfNumber = number - number.toInt()
+        if (decimalPartOfNumber == 0.0) {
+            var factorial = BigInteger("1")
+            for (i in 1..number.toInt()) {
+                factorial *= i.toBigInteger()
+            }
+            return factorial.toDouble()
+        } else {
+            return gammaLanczos(number+1)
         }
-        return factorial.toDouble()
     }
+
+    fun gammaLanczos(x: Double): Double {
+        // https://rosettacode.org/wiki/Gamma_function
+        var xx = x
+        val p = doubleArrayOf(
+            0.99999999999980993,
+            676.5203681218851,
+            -1259.1392167224028,
+            771.32342877765313,
+            -176.61502916214059,
+            12.507343278686905,
+            -0.13857109526572012,
+            9.9843695780195716e-6,
+            1.5056327351493116e-7
+        )
+        val g = 7
+        if (xx < 0.5) return Math.PI / (sin(Math.PI * xx) * gammaLanczos(1.0 - xx))
+        xx--
+        var a = p[0]
+        val t = xx + g + 0.5
+        for (i in 1 until p.size) a += p[i] / (xx + i)
+        return sqrt(2.0 * Math.PI) * t.pow(xx + 0.5) * exp(-t) * a
+    }
+
     fun evaluate(equation: String, isDegreeModeActivated: Boolean): Double {
         println("\n\n$equation")
         // https://stackoverflow.com/questions/3422673/how-to-evaluate-a-math-expression-given-in-string-form
