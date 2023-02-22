61c5737f7a9997fc99f30a2b9fde8d45a1efe076
Darkempire <50015928+Darkempire78@users.noreply.github.com>
2023-02-23 00:24:10 +0100

    update settings ui

--- app/src/main/java/com/darkempire78/opencalculator/SettingsActivity.kt
+++ app/src/main/java/com/darkempire78/opencalculator/SettingsActivity.kt
@@ -1,13 +1,27 @@
 package com.darkempire78.opencalculator
 
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceFragmentCompat
+import com.darkempire78.opencalculator.databinding.SettingsActivityBinding
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Themes
+        val themes = Themes(this)
+        themes.applyDayNightOverride()
+        setTheme(themes.getTheme())
+
+        // Change the status bar color
+        window.statusBarColor = R.attr.background_color
+
         setContentView(R.layout.settings_activity)
         if (savedInstanceState == null) {
             supportFragmentManager
@@ -16,6 +30,12 @@ class SettingsActivity : AppCompatActivity() {
                     .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // back button
+        findViewById<ImageView>(R.id.settings_back_button).setOnClickListener {
+            println("testtttt")
+            finish()
+        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
