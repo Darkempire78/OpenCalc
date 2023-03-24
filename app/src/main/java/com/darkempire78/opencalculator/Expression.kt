58811e4f5401e7c733e3585d4871e88113ab88bb
Darkempire <50015928+Darkempire78@users.noreply.github.com>
2023-03-24 17:44:26 +0100

    Update Expression.kt

--- app/src/main/java/com/darkempire78/opencalculator/Expression.kt
+++ app/src/main/java/com/darkempire78/opencalculator/Expression.kt
@@ -59,9 +59,9 @@ class Expression {
         if(calculation[operatorBeforePercentPos] == '/') {
             // insert brackets into percentage. Fixes 900/10% -> 900/(10/100), not 900/10/100 which evals differently.
             // also prevents it from doing the rest of this function, which screws the calculation up
-            var stringFirst = calculation.substring(0, operatorBeforePercentPos+1)
-            var stringMiddle = calculation.substring(operatorBeforePercentPos+1, percentPos+1)
-            var stringLast = calculation.substring(percentPos+1, calculation.length)
+            val stringFirst = calculation.substring(0, operatorBeforePercentPos+1)
+            val stringMiddle = calculation.substring(operatorBeforePercentPos+1, percentPos+1)
+            val stringLast = calculation.substring(percentPos+1, calculation.length)
             return "$stringFirst($stringMiddle)$stringLast"
         }
 
