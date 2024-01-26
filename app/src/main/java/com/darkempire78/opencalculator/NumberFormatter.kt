faac00b1a704822ce704596bb5e47f94917fc4db
Muraino Yakubu <muraino.yakubu@mavenmachines.com>
2024-01-26 14:35:26 +0000

    fix: In the original code, there were a few hiccups in handling number formatting, especially around the use of decimal and grouping separators. To address this, I revised the logic to accurately incorporate these separators into numbers. I also made sure that when numbers are being formatted and replaced back into the text, we only replace the first instance of each number to avoid duplicating changes. These tweaks ensure that numbers within any text are formatted correctly, maintaining the intended structure and readability.

--- app/src/main/java/com/darkempire78/opencalculator/NumberFormatter.kt
+++ app/src/main/java/com/darkempire78/opencalculator/NumberFormatter.kt
@@ -2,39 +2,56 @@ package com.darkempire78.opencalculator
 
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
+import java.util.Locale
 
 object NumberFormatter {
-    fun format(text: String, decimalSeparatorSymbol : String, groupingSeparatorSymbol : String): String {
+    fun format(
+        text: String,
+        decimalSeparatorSymbol: String,
+        groupingSeparatorSymbol: String
+    ): String {
         val textNoSeparator = removeSeparators(text, groupingSeparatorSymbol)
         val numbersList = extractNumbers(textNoSeparator, decimalSeparatorSymbol)
-        val numbersWithSeparators = addSeparators(numbersList, decimalSeparatorSymbol)
+        val numbersWithSeparators =
+            addSeparators(numbersList, decimalSeparatorSymbol, groupingSeparatorSymbol)
         var textWithSeparators = textNoSeparator
         numbersList.forEachIndexed { index, number ->
-            textWithSeparators = textWithSeparators.replace(number, numbersWithSeparators[index])
+            textWithSeparators =
+                textWithSeparators.replaceFirst(number, numbersWithSeparators[index])
         }
         return textWithSeparators
     }
 
-    fun extractNumbers(text: String, decimalSeparatorSymbol : String): List<String> {
-        val numberRegex = "(\\d+\\$decimalSeparatorSymbol\\d+)|(\\d+\\$decimalSeparatorSymbol)|(\\$decimalSeparatorSymbol\\d+)|(\\$decimalSeparatorSymbol)|(\\d+)".toRegex()
+    fun extractNumbers(text: String, decimalSeparatorSymbol: String): List<String> {
+        val numberRegex =
+            "(\\d+\\$decimalSeparatorSymbol\\d+)|(\\d+\\$decimalSeparatorSymbol)|(\\$decimalSeparatorSymbol\\d+)|(\\$decimalSeparatorSymbol)|(\\d+)".toRegex()
 
         val results = numberRegex.findAll(text)
         return results.map { it.value }.toList()
     }
 
-    private fun addSeparators(numbersList: List<String>, decimalSeparatorSymbol : String): List<String> {
+    private fun addSeparators(
+        numbersList: List<String>,
+        decimalSeparatorSymbol: String,
+        groupingSeparatorSymbol: String
+    ): List<String> {
+        val symbols = DecimalFormatSymbols(Locale.getDefault()).apply {
+            decimalSeparator = decimalSeparatorSymbol[0]
+            groupingSeparator = groupingSeparatorSymbol[0]
+        }
+        val decimalFormat = DecimalFormat("#,###", symbols)
+
         return numbersList.map {
             if (it.contains(decimalSeparatorSymbol)) {
                 if (it.first() == decimalSeparatorSymbol[0]) {
-                    //this means the floating point number doesn't have integers
                     it
                 } else {
                     val integersPart = it.substring(0, it.indexOf(decimalSeparatorSymbol))
                     val fractions = it.substring(it.indexOf(decimalSeparatorSymbol) + 1)
-                    DecimalFormat().format(integersPart.toBigDecimal()) + decimalSeparatorSymbol + fractions
+                    decimalFormat.format(integersPart.toBigDecimal()) + decimalSeparatorSymbol + fractions
                 }
             } else {
-                DecimalFormat().format(it.toBigDecimal())
+                decimalFormat.format(it.toBigDecimal())
             }
         }
     }
