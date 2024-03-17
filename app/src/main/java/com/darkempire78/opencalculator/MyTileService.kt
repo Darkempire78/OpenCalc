ca94f70f6da23c60cc384bf3ba359d112799d483
Muraino Yakubu <murainoy@yahoo.com>
2024-03-17 13:28:33 +0000

    updated TileService to use the google guide on using pending intent

--- app/src/main/java/com/darkempire78/opencalculator/MyTileService.kt
+++ app/src/main/java/com/darkempire78/opencalculator/MyTileService.kt
@@ -1,17 +1,28 @@
 package com.darkempire78.opencalculator
 
+import android.app.PendingIntent
 import android.content.Intent
+import android.os.Build
 import android.service.quicksettings.TileService
 
 class MyTileService: TileService() {
-    companion object {
-        private const val BROADCAST_ACTION = "darkempire78.opencalculator.action.START_ACTIVITY"
-    }
 
     // Called when the user taps on your tile in an active or inactive state.
     override fun onClick() {
         super.onClick()
-        val intent = Intent(BROADCAST_ACTION)
-        sendBroadcast(intent)
+
+        // Intent to launch MainActivity
+        val intent = Intent(this, MainActivity::class.java)
+        // Create a PendingIntent from the intent
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+
+        // Check the SDK version to determine which method to use
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startActivityAndCollapse(pendingIntent)
+        } else {
+            // For older versions, convert PendingIntent to Intent and start the activity
+            val newIntent = Intent(intent).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(newIntent)
+        }
     }
 }
