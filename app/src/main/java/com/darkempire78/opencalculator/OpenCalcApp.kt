dc5423acf3b823dcd97923b53a26fa3f9d82b339
Darkempire <50015928+Darkempire78@users.noreply.github.com>
2024-07-08 20:14:59 +0200

    add font setting

--- app/src/main/java/com/darkempire78/opencalculator/OpenCalcApp.kt
+++ app/src/main/java/com/darkempire78/opencalculator/OpenCalcApp.kt
@@ -1,12 +1,20 @@
 package com.darkempire78.opencalculator
 
+import android.app.Activity
 import android.app.Application
+import android.graphics.Typeface
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate.*
 
 class OpenCalcApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        FontManager(this).applyFont(this)
 
         // if the theme is overriding the system, the first creation doesn't work properly
         val forceDayNight = MyPreferences(this).forceDayNight
